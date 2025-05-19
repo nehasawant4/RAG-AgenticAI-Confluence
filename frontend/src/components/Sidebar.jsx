f8e4bcec99df7aaf5eabe95577f7eae8395216f7
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2025-05-19 00:37:13 -0700

    UI

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ function Sidebar({ activeTab, setActiveTab, isVisible }) {
     <aside className={`sidebar ${!isVisible ? 'hidden' : ''}`}>
       <div className="logo" onClick={() => setActiveTab('')}>
         <div className="logo-container">
-          <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0,0,256,256">
+          <svg className="logo-icon" xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0,0,256,256">
             <g fill="#63da95" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{mixBlendMode: "normal"}}>
               <g transform="scale(10.66667,10.66667)">
                 <path d="M12,2.09961l-11,9.90039h3v9h7v-6h2v6h7v-9h3zM12,4.79102l6,5.40039v0.80859v8h-3v-6h-6v6h-3v-8.80859z"></path>
